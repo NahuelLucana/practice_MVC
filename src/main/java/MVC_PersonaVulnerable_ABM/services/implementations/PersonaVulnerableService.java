d338133304e5840bec9075d3bddcd198a915b9d8
NahuelLucana <nlucanadelgadillo@frba.utn.edu.ar>
2024-06-12 17:34:16 -0300

    Agrego un tipo test real de como funciona el flujo de ejecución desde el controller con la llamada al crear() hasta su retorno
    Nota: Puede que este quemado pero demasiadas CAPAS, pero es rentable a largo plazo, y es muy cohesivo cada capa por lo tanto vale la pena, pero es dificil de seguir.
    Estas son las capas de lo más alto a lo bajo:
    Controller -> Service -> Repositorio -> DAO -> Medio persistente
    Tambien aparece el DTO que el Data Transfer Object que sirve entre el controller y el service.
    Tambien aparece el DAO en consecuencia de usar el Repository Pattern combinado con el Strategy Pattern, en el cual el DAO es el responsable de acceder al medio persistente. DAO: Data Access Object

--- src/main/java/MVC_PersonaVulnerable_ABM/services/implementations/PersonaVulnerableService.java
+++ src/main/java/MVC_PersonaVulnerable_ABM/services/implementations/PersonaVulnerableService.java
@@ -19,25 +19,29 @@ public class PersonaVulnerableService implements IPersonaVulnerableService {
     }
     @Override
     public PersonaVulnerableOutputDTO crear(PersonaVulnerableInputDTO persona){
+        System.out.println("Ejecutando el crear() del service...");
 
         //Se puede hacer validaciones de usuario, de la persona, etc.
 
         ////Guardado hacia el repositorio
 
         //Creo la instancia de personaVulnerable
+        System.out.println("Instanciando la persona vulnerable para ser guardado en su respectivo repositorio...");
         LocalDate fechaDeRegistro = LocalDate.now();
         PersonaVulnerable personaVulnerableAGuardar = new PersonaVulnerable(persona.getNombre(),persona.getNacimiento());
         personaVulnerableAGuardar.setFechaRegitrado(fechaDeRegistro);
-
+        System.out.println("Finaliza la instanciación.");
         //Guardo la instancia de persona vulnerable hacia el repositorio
+        System.out.println("Ejecutando la llamada al guardar() del repositorio desde el service para el guardado de: " + personaVulnerableAGuardar.getNombre()+ " ..." );
         personaVulnerableRepository.guardar(personaVulnerableAGuardar); //Ya esta testeado asi que si quiero testear este service puedo mockear el repositorio.
-
+        System.out.println("Finaliza la llamada guardar() al repositorio desde el service.");
         //OutputDTO
+        System.out.println("Inicio de instanciación y configuración de outputDTO");
         PersonaVulnerableOutputDTO outputDTO = new PersonaVulnerableOutputDTO();
         outputDTO.setNombre(persona.getNombre());
         outputDTO.setFechaNacimiento(persona.getNacimiento());
         outputDTO.setFechaDeRegistro(fechaDeRegistro);
-
+        System.out.println("Finaliza instanciación y se procede a retornar el outputDTO");
         return outputDTO;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,37 @@
 package org.example;
 
+import MVC_PersonaVulnerable_ABM.controllers.PersonaVulnerableController;
+import MVC_PersonaVulnerable_ABM.models.repositories.implementation.PersonasVulnerableRepository;
+import MVC_PersonaVulnerable_ABM.models.repositories.interfaces.IPersonaVulnerableRepository;
+import MVC_PersonaVulnerable_ABM.services.dtos.inputs.PersonaVulnerableInputDTO;
+import MVC_PersonaVulnerable_ABM.services.dtos.outputs.PersonaVulnerableOutputDTO;
+import MVC_PersonaVulnerable_ABM.services.implementations.PersonaVulnerableService;
+import MVC_PersonaVulnerable_ABM.services.interfaces.IPersonaVulnerableService;
+
+import java.time.LocalDate;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private PersonaVulnerableOutputDTO outputDTO;
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Instanciaciones
+        IPersonaVulnerableRepository repository = new PersonasVulnerableRepository(); //Por default tiene el medio de persistencia de memoria
+        IPersonaVulnerableService service = new PersonaVulnerableService(repository);
+        PersonaVulnerableController controller = new PersonaVulnerableController(service);
+        PersonaVulnerableInputDTO solicitud = new PersonaVulnerableInputDTO();
+        solicitud.setNombre("Nayeon");
+        solicitud.setNacimiento(LocalDate.of(1996,5,27));
+        System.out.println("Solicitud:");
+        System.out.println(solicitud.getNombre());
+        System.out.println(solicitud.getNacimiento());
+        //Controller recibe solicitud(por ahora es un object)
+        Object ouput = controller.crear(solicitud);
+        PersonaVulnerableOutputDTO ouputDTO = (PersonaVulnerableOutputDTO) ouput;
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Output:");
+        System.out.println(ouputDTO.getNombre());
+        System.out.println(ouputDTO.getFechaNacimiento());
+        System.out.println(ouputDTO.getFechaDeRegistro());
     }
 }
